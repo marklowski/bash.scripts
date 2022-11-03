fa47e4177cc58932597279f8645dedae358e0a0f
Philipp von Marklowski <philipp.vonmarklowski@pr-p.net>
2022-11-03 23:57:54 +0100

    refactor(raspiRepositorySelector): add new Color Include

--- raspiRepositorySelector.sh
+++ raspiRepositorySelector.sh
@@ -1,14 +1,7 @@
 #! /bin/bash
 
-# ASCII Coloring
-_RED="\e[0;31m"
-_CYAN="\e[0;36m"
-_YELLOW="\e[0;33m"
-_BLUE="\e[0;34m"
-_PURPLE="\e[0;35m"
-_WHITE="\e[0;37m"
-_BOLD="\e[1m"
-_RESET="\e[0m"
+# Color Include
+source ~/extSSD/p/projects/bash.scripts/incl.Colors.sh
 
 # Source SSH Settings
 source ~/.config/script-settings/sshData.cfg
@@ -20,12 +13,12 @@ echo "$_REPOS" >> test.txt
 #while getopts ":hcle:r:C:" opt; do
 #case ${opt} in
 #		h )
-#			echo -e "${_CYAN}${_BOLD}Listing Help: ${_RESET}"
-#			echo -e "${_WHITE}${_BOLD}-l: ${_RESET} List Local Changes"
-#			echo -e "${_WHITE}${_BOLD}-r: ${_RESET} List Remote Changes(Work=W, Home=H)"
-#			echo -e "${_WHITE}${_BOLD}-c: ${_RESET} Clear Local File"
-#			echo -e "${_WHITE}${_BOLD}-C: ${_RESET} Clear Remote File (Work=W, Home=H)"
-#			echo -e "${_WHITE}${_BOLD}-e: ${_RESET} Execute Program\n"
+#			echo -e "${_FG_CYAN}${_TX_BOLD}Listing Help: ${_TX_RESET}"
+#			echo -e "${_FG_WHITE}${_TX_BOLD}-l: ${_TX_RESET} List Local Changes"
+#			echo -e "${_FG_WHITE}${_TX_BOLD}-r: ${_TX_RESET} List Remote Changes(Work=W, Home=H)"
+#			echo -e "${_FG_WHITE}${_TX_BOLD}-c: ${_TX_RESET} Clear Local File"
+#			echo -e "${_FG_WHITE}${_TX_BOLD}-C: ${_TX_RESET} Clear Remote File (Work=W, Home=H)"
+#			echo -e "${_FG_WHITE}${_TX_BOLD}-e: ${_TX_RESET} Execute Program\n"
 #			exit 1
 #			;;
 #		c )
@@ -48,11 +41,11 @@ echo "$_REPOS" >> test.txt
 #			clearConfigRemote
 #			;;
 #		\? )
-#			echo -e "${_YELLOW}${_BOLD}Invalid Option: ${_RESET} $OPTARG" 1>&2
+#			echo -e "${_FG_YELLOW}${_TX_BOLD}Invalid Option: ${_TX_RESET} $OPTARG" 1>&2
 #			exit 1
 #			;;
 #		: )
-#            echo -e "${_YELLOW}${_BOLD}Invalid Option: ${_RESET} -$OPTARG requires an argument \n" 1>&2
+#            echo -e "${_FG_YELLOW}${_TX_BOLD}Invalid Option: ${_TX_RESET} -$OPTARG requires an argument \n" 1>&2
 #            exit 1
 #            ;;
 #	esac
