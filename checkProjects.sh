401a3a05d46ceb45e7a320d0003206f94d3eb6b9
Philipp von Marklowski <philipp.vonmarklowski@pr-p.net>
2022-11-03 22:51:56 +0100

    refactor(checkProjects): add new Color Include

--- checkProjects.sh
+++ checkProjects.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-# Check for Commits
+# Check Git Projects
 
-# ASCII Coloring
-_RED="\e[0;31m"
-_CYAN="\e[0;36m"
-_YELLOW="\e[0;33m"
-_BLUE="\e[0;34m"
-_PURPLE="\e[0;35m"
-_WHITE="\e[0;37m"
-_BOLD="\e[1m"
-_RESET="\e[0m"
+# Color Include
+source ~/extSSD/p/projects/bash.scripts/incl.Colors.sh
 
 # Global Variables
 _CONFIG_FILE="$HOME/.config/script-settings/chkProjects.cfg"
@@ -25,7 +18,7 @@ clear # Clear Screen
 for line in $_PROJECTS
 do
 	GITBASED=.git
-	echo -e "${_WHITE}${_BOLD}Checked Folder:${_RESET} $line"
+	echo -e "${_FG_WHITE}${_TX_BOLD}Checked Folder:${_TX_RESET} $line"
 
 	# Loop all sub-directories
 	for directories in $line*
@@ -38,24 +31,24 @@ do
 				test=$(git status);
 				# Check Local Repository for changes
 				if [[ $test == *"nothing to commit"* ]]; then
-					msg="${_CYAN} No Changes "
+					msg="${_FG_CYAN} No Changes "
 					# Check if git status has unstaged changes
 				elif [[ $test == *"Changes not staged for commit"* ]]; then
-					msg="${_YELLOW} Unstaged changes "
+					msg="${_FG_YELLOW} Unstaged changes "
 					# Check if git status has uncommitted changes
 				elif [[ $test == *"Untracked files"* ]]; then
-					msg="${_RED} You forgot to commit some files "
+					msg="${_FG_RED} You forgot to commit some files "
 				elif [[ $test == *"Changes to be committed"* ]]; then
-					msg="${_PURPLE} Staged Changes but not Commited "
+					msg="${_FG_PURPLE} Staged Changes but not Commited "
 				fi
 
-				echo -e "${directories##*/}:$msg${_RESET}"
+				echo -e "${directories##*/}:$msg${_TX_RESET}"
 				cd ..
 			fi
 			[ $_NO_DIRECTORIES = true ] && _NO_DIRECTORIES=false
 		fi
 	done
-	[ $_NO_DIRECTORIES = true ] && echo -e "${_WHITE}${_BOLD}INFO:${_RESET}${_BLUE} No Directories Found ${_RESET}" || _NO_DIRECTORIES=true
+	[ $_NO_DIRECTORIES = true ] && echo -e "${_FG_WHITE}${_TX_BOLD}INFO:${_TX_RESET}${_FG_BLUE} No Directories Found ${_TX_RESET}" || _NO_DIRECTORIES=true
 	echo ""
 done
 
