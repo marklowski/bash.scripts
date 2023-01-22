2a79bf7a72020e78124a86de27731750e67f0f68
Philipp von Marklowski <philipp.vonmarklowski@pr-p.net>
2023-01-22 22:41:26 +0100

    refactor(transportScript): general refactoring of the script

--- transportScript.sh
+++ transportScript.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 # Build Transport Script Path
 
-# Color Include
-source $BASH_COLOR_INCL
-
-# Global Variables
+#
+# Include's
+#
 _CONFIG_FILE_OPTIONS="$HOME/.config/script-settings/transportScriptOptions.cfg"
 _CONFIG_FILE_PATH="$HOME/.config/script-settings/transportScriptPath.cfg"
+source $BASH_COLOR_INCL
 
+#
+# Global Variables
+#
 _TRANSPORT_NUMBER="DRPK902330"
 _TRANSPORT_SHORTCUT="" # possible values "K" or "R"
 
@@ -19,11 +22,11 @@ _SELECTED_ENTRY=0
 _OUTPUT_ENTRY=0
 _REVERSE_OUTPUT='False'
 
-# Read User Configurable Options & add exit option
+# read User Configurable Options & add exit option
 mapfile -t optionsArray < $_CONFIG_FILE_OPTIONS
 _ARRAY_COLUMNS=$(( ${#optionsArray[@]} + $_PRP_ENTRY ))
 
-# Source Possible PATH Settings
+# source Possible PATH Settings
 mapfile -t -d ";" pathArrayComplete < $_CONFIG_FILE_PATH
 
 declare -A pathArray
@@ -45,12 +48,15 @@ for item in "${pathArrayComplete[@]}"; do
     fi
 done
 
+#
+# return the corresponding results.
+#
 returnResults() {
     # set cofiles options
     _TRANSPORT_SHORTCUT="K"
 
     # output cofiles header
-    echo -e "${_FG_CYAN}Transports:  ${_TX_BOLD}$_TRANSPORT_SHORTCUT ${_TX_RESET}"
+    echo -e "${_FG_CYAN}Transports:  $_TRANSPORT_SHORTCUT ${_TX_RESET}"
 
     # output paths's
     returnResult
@@ -59,12 +65,15 @@ returnResults() {
     _TRANSPORT_SHORTCUT="R"
 
     # output data header
-    echo -e "${_FG_CYAN}Transports:  ${_TX_BOLD}$_TRANSPORT_SHORTCUT ${_TX_RESET}"
+    echo -e "${_FG_CYAN}Transports:  $_TRANSPORT_SHORTCUT ${_TX_RESET}"
 
     # output paths's
     returnResult
 }
 
+#
+# return the results for one option.
+#
 returnResult() {
     # split imported TA Number
     sapSystem=${_TRANSPORT_NUMBER::3}
@@ -97,27 +106,9 @@ returnResult() {
     echo -e ""
 }
 
-pathArrayDebug() {
-    echo
-    f1="%$((${#num_rows}+1))s"
-    f2=" %9s"
-
-    printf "$f1" ''
-    for ((i=1;i<=num_rows;i++)) do
-        printf "$f2" $i
-    done
-
-    echo
-    for ((j=1;j<=$_ARRAY_COLUMNS;j++)) do
-        printf "$f1" $j
-        for ((i=1;i<=$_ARRAY_ROWS;i++)) do
-            printf "$f2" ${pathArray[$i,$j]}
-        done
-        echo
-    done
-}
-
-
+#
+# get the corresponding transport path from config, according to user input.
+#
 getTransportPath() {
     PS3="SAP System wählen: "
     select option in "${optionsArray[@]}"; do
@@ -130,81 +121,85 @@ getTransportPath() {
     done
 
     echo ""
-    echo -e "${_FG_BLUE}Displaying Transport Paths for ${_TX_BOLD}$_TRANSPORT_NUMBER${_TX_RESET}"
+    echo -e "${_FG_BLUE}Displaying Transport Paths for $_TRANSPORT_NUMBER${_TX_RESET}"
     
     echo -e ""
-    echo -e "${_FG_YELLOW}Local System: ${_TX_BOLD}PRP${_TX_RESET}"
+    echo -e "${_FG_YELLOW}Local System: PRP${_TX_RESET}"
     _OUTPUT_ENTRY=$_PRP_ENTRY
     _REVERSE_OUTPUT="false"
     returnResults
 
     echo ""
-    echo -e "${_FG_YELLOW}Target System: ${_TX_BOLD}$option${_TX_RESET}"
+    echo -e "${_FG_YELLOW}Target System: $option${_TX_RESET}"
     _OUTPUT_ENTRY=$_SELECTED_ENTRY
     _REVERSE_OUTPUT="True"
     returnResults
 
 }
 
+#
+# check if there are undocumented transport files available,
+# and execute accordingly.
+#
 checkAndDocumentTransports() {
     logTransports.sh -c
     returnValue=$?
 
     if [ $returnValue == 0 ]; then
         echo ""
-        while true
-        do
+        while true; do
             read -e -p "Document Previous Transport [Y/n]: " input
             echo ""
 
             case $input in
                 [yY][eE][sS]|[yY])
                     logTransports.sh -s
-                    echo -e "${_FG_YELLOW}${_TX_BOLD}Finished Sub-Script ${_TX_RESET} \n" 1>&2
+                    echo -e "${_FG_YELLOW}Finished Sub-Script ${_TX_RESET} \n" 1>&2
                     break
                     ;;
                 [nN][oO]|[nN])
-                    echo -e "${_FG_BLUE}${_TX_BOLD}Continuing without Sub-Script ${_TX_RESET} \n" 1>&2
+                    echo -e "${_FG_BLUE}Continuing without Sub-Script ${_TX_RESET} \n" 1>&2
                     break
                     ;;
                 *)
-                    echo -e "${_FG_RED}${_TX_BOLD}Invalid input... ${_TX_RESET} \n" 1>&2
+                    echo -e "${_FG_RED}Invalid input... ${_TX_RESET} \n" 1>&2
                     ;;
             esac
         done
     fi
 }
 
-while getopts ":hg:ad" opt; do
-	case ${opt} in
-		h )
-			echo -e "${_FG_CYAN}${_TX_BOLD}Listing Help: ${_TX_RESET}"
-			echo -e " ${_FG_WHITE}${_TX_BOLD}-g: ${_TX_RESET} get Transport Paths"
-			echo -e " ${_FG_WHITE}${_TX_BOLD}-a: ${_TX_RESET} add new Transport Path"
-			echo -e " ${_FG_WHITE}${_TX_BOLD}-d: ${_TX_RESET} delete a Transport Path"
-            exit 1
-            ;;
-		g )
-            checkAndDocumentTransports
+#
+# output script description.
+#
+printHelp() {
+  echo -e "${_FG_CYAN}Listing Help: ${_TX_RESET}"
+  echo -e "${_SPACE_2}${_FG_WHITE}-g: ${_TX_RESET} get Transport Paths"
+  echo -e "${_SPACE_4}${_FG_YELLOW}args${_TX_RESET} - Transport Number (e.g. DRPK902330)"
+  echo -e "${_SPACE_2}${_FG_RED}-a: ${_TX_RESET} add new Transport Path"
+  echo -e "${_SPACE_2}${_FG_RED}-d: ${_TX_RESET} delete a Transport Path"
+  echo ""
+  echo -e "${_FG_CYAN}Additional Information: ${_TX_RESET}"
+  echo -e "${_SPACE_2}Option -g can be passed multiple times."
+}
 
-            transportNumbers+=("$OPTARG");;
-		a )
-			echo -e "${_FG_YELLOW}${_TX_BOLD}Option is not Implemented${_TX_RESET} \n" 1>&2
-			;;
-		d )
-			echo -e "${_FG_YELLOW}${_TX_BOLD}Option is not Implemented${_TX_RESET} \n" 1>&2
-			;;
-		\? ) echo -e "${_FG_YELLOW}${_TX_BOLD}Unknown Option: ${_TX_RESET} -$OPTARG" >&2; exit 1;;
-		:  ) echo -e "${_FG_YELLOW}${_TX_BOLD}Missing option argument for ${_TX_RESET} -$OPTARG" >&2; exit 1;;
-		*  ) echo -e "${_FG_RED}${_TX_BOLD}Unimplemented Option: ${_TX_RESET} -$OPTARG" >&2; exit 1;;
+#
+# handle script options.
+#
+while getopts ":hg:ad" opt; do
+  case ${opt} in
+    g ) checkAndDocumentTransports; transportNumbers+=("$OPTARG") ;;
+    a ) echo -e "${_FG_YELLOW}Option is not Implemented${_TX_RESET} \n" 1>&2 ;;
+    d ) echo -e "${_FG_YELLOW}Option is not Implemented${_TX_RESET} \n" 1>&2 ;;
+    h  ) printHelp exit 1;;
+    \? ) echo -e "${_FG_YELLOW}Unknown Option: ${_TX_RESET} -$OPTARG" >&2; exit 1;;
+    :  ) echo -e "${_FG_YELLOW}Missing option argument for ${_TX_RESET} -$OPTARG" >&2; exit 1;;
+    *  ) echo -e "${_FG_RED}Unimplemented Option: ${_TX_RESET} -$OPTARG" >&2; exit 1;;
 	esac
 done
 
-if ((OPTIND == 1))
-then
-echo -e "${_FG_RED}${_TX_BOLD}Error: No Option specified ${_TX_RESET}" >&2;
-fi
-
+# Standard Behaviour when, no option was supplied.
+if ((OPTIND == 1)); then printHelp; fi
 shift $((OPTIND -1))
 
 for transportNumber in "${transportNumbers[@]}"; do
