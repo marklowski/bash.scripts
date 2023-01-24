4aa5199975dbb44d1ef8d16d6d90792e4bf16a84
Philipp von Marklowski <P.Marklowski@flexus.net>
2023-01-24 16:27:49 +0100

    feat(setupDotFiles): First Version of Script

---
+++ setupDotFiles.sh
@@ -0,0 +1,163 @@
+#!/bin/bash
+# Setup dotFiles Directory.
+
+#
+# Include's
+#
+_DOTFILES_PATH=$HOME/dotFiles
+source $BASH_COLOR_INCL
+
+#
+# Global Variables
+#
+_QUICK_SELECT=false
+_SELECTED_ITEM_INDEX=""
+_TARGET_DIRECTORY=./.config
+
+declare -a _DIRECTORIES
+declare -a _DIRECTORIES_SHORTEND
+declare -a _FILES
+
+#
+# set corresponding system directory.
+#
+setSystemDirectory() {
+  PS3="dotFile System Ordner wählen: "
+
+  select option in "${_DIRECTORIES[@]##*/}"; do
+    for item in "${_DIRECTORIES[@]##*/}"; do
+      if [[ $item == $option ]]; then
+        echo -e "Proceeding with ${_FG_BLUE}${item^^}${_TX_RESET}\n"
+        _SELECTED_ITEM_INDEX=$REPLY
+        break 2
+      fi
+    done
+  done
+}
+
+#
+# get the configured system directories.
+#
+getSystemDirectories() {
+  systemDirectory=$1
+  directoryCounter=1
+
+  # loop over dotFiles directory.
+  for entry in "$_DOTFILES_PATH"/*; do
+      
+    # when directory found add to array.
+    if [ -d "$entry" ]; then
+      _DIRECTORIES[$directoryCounter]=$entry
+
+      # when quick select active, check if userInput is available & break;
+      # otherwise send setSystemDirectory Dialog
+      if $_QUICK_SELECT; then
+        if [[ ${entry##*/} == $systemDirectory ]]; then 
+          echo -e "Proceeding with ${_FG_BLUE}${systemDirectory^^}${_TX_RESET}\n"
+
+          _SELECTED_ITEM_INDEX=directoryCounter; 
+          break;
+        fi
+      fi
+
+      directoryCounter=$(($directoryCounter+1))
+    fi
+  done
+
+  # when Item was pre selected handle standard dialog
+  if [[ $_SELECTED_ITEM_INDEX == "" ]]; then
+    if $_QUICK_SELECT; then
+      echo -e "${_FG_RED}Error:${_TX_RESET} The Argument ${systemDirectory^^} wasn't found!\n"
+    fi
+    setSystemDirectory
+  fi
+}
+
+#
+# check or create directories.
+#
+prepareDirectories() {
+
+  mkdir ./.config
+#  # check for .config directory
+#  if [ ! -d "$HOME/.config" ]; then
+#    mkdir -p $HOME/.config
+#  fi
+#
+#  # check .local/bin, not relevent for dotFiles but for bash.scripts
+#  if [ ! -d "$HOME/.local/bin" ]; then
+#    mkdir -p $HOME/.local/bin
+#  fi
+
+  echo -e "${_FG_BLUE}Info (1/3):${_TX_RESET} Directory prepartions complete!\n"
+}
+
+checkSystemDirectory() {
+  checkDirectory=$1
+
+  # check if system Directory/.config has the corresponding directory
+  for subEntry in "${_DIRECTORIES[$_SELECTED_ITEM_INDEX]}"/.config/*; do
+    if [[ "${subEntry##*/}" == "${checkDirectory}" ]]; then
+      return 1
+    fi
+  done
+}
+#
+# link dotFiles/.config directories, while checking system directory.
+#
+linkDotConfig() {
+  
+  # loop over dotFiles directory.
+  for entry in "$_DOTFILES_PATH"/.config/*; do
+      
+    # when directory found add to array.
+    if [ -d "$entry" ]; then
+      checkSystemDirectory ${entry##*/}
+      returnValue=$?
+
+      if [ $returnValue == 1 ]; then
+        echo $subEntry
+        ln -sf $subEntry $_TARGET_DIRECTORY
+      else
+        echo $entry
+        ln -sf $entry $_TARGET_DIRECTORY
+      fi
+    fi
+  done
+}
+
+main() {
+  systemDirectory=$1
+
+  getSystemDirectories $systemDirectory
+
+  prepareDirectories
+
+  linkDotConfig
+}
+#
+# output script description.
+#
+printHelp() {
+  echo -e "${_FG_CYAN}Listing Help: ${_TX_RESET}"
+  echo -e "${_SPACE_2}${_FG_WHITE}-e: ${_TX_RESET} Normal Execution Mode (with Dialog)"
+  echo -e "${_SPACE_2}${_FG_WHITE}-q: ${_TX_RESET} Quick Execution Mode (Dialog Fallback)"
+}
+
+#
+# handle script options.
+#
+while getopts ":heq:" opt; do
+	case ${opt} in
+		e  ) main ;;
+    q  ) _QUICK_SELECT=true; main "$OPTARG" ;;
+		h  ) printHelp exit 1;;
+		\? ) echo -e "${_FG_YELLOW}Unknown Option: ${_TX_RESET} -$OPTARG" >&2; exit 1;;
+		:  ) echo -e "${_FG_YELLOW}Missing option argument for ${_TX_RESET} -$OPTARG" >&2; exit 1;;
+		*  ) echo -e "${_FG_RED}Unimplemented Option: ${_TX_RESET} -$OPTARG" >&2; exit 1;;
+	esac
+done
+
+# Standard Behaviour when, no option was supplied.
+if ((OPTIND == 1)); then main; fi
+shift $((OPTIND -1))
