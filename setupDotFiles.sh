bffa2c191580a13671d8704e640ab0cc86b39e58
Philipp von Marklowski <philipp.vonmarklowski@pr-p.net>
2023-01-27 20:39:16 +0100

    feat(setupDotFiles): adds link functionality for files & adds logging for different steps

--- setupDotFiles.sh
+++ setupDotFiles.sh
@@ -89,8 +89,6 @@ prepareDirectories() {
   if [ ! -d "$HOME/.local/bin" ]; then
     mkdir -p $HOME/.local/bin
   fi
-
-  echo -e "${_FG_BLUE}$i_mdi_information_variant Info (1/3):${_TX_RESET} Directory prepartions complete!\n"
 }
 
 #
@@ -112,16 +110,59 @@ checkConfigDirectory() {
 #
 # @param(checkDirectory) - expects the path to the file with '/' at the end of it.
 # @param(checkEntry) - corresponding entry that has to be checked.
-# @param()
-checkHasTags() {
-  checkDirectory=$1
+# @param(isDirectory) - different handling for directory/files.
+#
+checkHasTags(){
+  directoryPath=$1
   checkEntry=$2
+  isDirectory=$3
+  linkEntry=""
 
-    # check possible entry 'directory path / cut entry at first . and add .*' with 
-  for subEntry in "${checkDirectory}${checkEntry%%'.'*}".*; do
-    echo $subEntry
-    ignoreDirectories+=("$subEntry")
-  done
+  declare -a tagArray
+
+  if $isDirectory; then
+
+    # check possible entry directory path / cut entry at first . and add .*' with
+    for subEntry in "${directoryPath}${checkEntry%%'.'*}".*; do
+      ignoreDirectories+=("$subEntry")
+      tagArray+=("${subEntry##*.}")
+    done
+
+    # pick from possible tags
+    PS3="Ein Variante vom Ordner ${_FG_BLUE}'${checkEntry%%'.'*}'${_TX_RESET} wählen: "
+    select option in "${tagArray[@]}"; do
+      for item in "${tagArray[@]}"; do
+        if [[ $item == $option ]]; then
+          pickedEntry="${checkEntry%%'.'*}.$item"
+          break 2
+        fi
+      done
+    done
+
+    echo ""
+  else
+    preparedEntry=${checkEntry:1}
+
+      # check possible entry 'directory path / cut entry at first . and add .*' with
+      for subEntry in "${directoryPath}.${preparedEntry%%'.'*}".*; do
+        if [[ $subEntry != *"*"* ]]; then
+          tagArray+=("${subEntry##*.}")
+          ignoreFiles+=("$subEntry")
+        fi
+      done
+
+    # pick from possible tags
+    PS3="Ein Variante von der Datei ${_FG_BLUE}'${preparedEntry%%'.'*}'${_TX_RESET} wählen: "
+      select option in "${tagArray[@]}"; do
+        for item in "${tagArray[@]}"; do
+          if [[ $item == $option ]]; then
+            pickedEntry=".${preparedEntry%%'.'*}.$item"
+            break 2
+          fi
+        done
+      done
+      echo ""
+  fi
 }
 
 #
@@ -138,11 +179,10 @@ linkConfigDirectory() {
       returnValue=$?
 
       if [ $returnValue == 1 ]; then
-        #ln -sf $subEntry $_TARGET_DIRECTORY
+        ln -sf $subEntry $_TARGET_DIRECTORY/
         ignoreDirectories+=("$subEntry")
       else
-        abc=1
-        #ln -sf $entry $_TARGET_DIRECTORY
+        ln -sf $entry $_TARGET_DIRECTORY/
       fi
     fi
   done
@@ -153,23 +193,18 @@ linkConfigDirectory() {
     # check if entry is within already copied directories.
     if [[ ! ${ignoreDirectories[*]} =~ $entry ]]; then
 
-      checkHasTags "${_DIRECTORIES[$_SELECTED_ITEM_INDEX]}/.config/" ${entry##*/}
+      pickedEntry=""
+      checkHasTags "${_DIRECTORIES[$_SELECTED_ITEM_INDEX]}/.config/" ${entry##*/} true
+
+      if [[ $pickedEntry != "" ]]; then
+        ln -sf ${_DIRECTORIES[$_SELECTED_ITEM_INDEX]}/.config/$pickedEntry "$_TARGET_DIRECTORY/${pickedEntry%%'.'*}"
+      fi
     fi
   done
-  echo -e "${_FG_BLUE}$i_mdi_information_variant Info (2/3):${_TX_RESET} Linkings .config Directory was completed!\n"
 }
 
 #
 # check if multiple files.
-#
-checkConfigFiles() {
-  checkFile=$1
-
- echo "$checkFile"
-}
-
-#
-# link the different config files.
 #
 linkConfigFiles() {
 
@@ -177,11 +212,20 @@ linkConfigFiles() {
   for entry in "${_DIRECTORIES[$_SELECTED_ITEM_INDEX]}"/.*; do
 
     if [ -f "$entry" ]; then
-      checkConfigFiles ${entry##*/}
+      if ([[ ! $entry == *".aliases"* ]] && [[ ! $entry == *".zsh-keymapping"* ]]); then
+        if [[ ! ${ignoreFiles[*]} =~ $entry ]]; then
+          pickedEntry=""
+          checkHasTags "${_DIRECTORIES[$_SELECTED_ITEM_INDEX]}/" ${entry##*/} false
+
+          if [[ $pickedEntry != "" ]]; then
+            ln -sf "${_DIRECTORIES[$_SELECTED_ITEM_INDEX]}/$pickedEntry" "./.${preparedEntry%%'.'*}"
+          else
+            ln -sf "$entry" ./
+          fi
+        fi
+      fi
     fi
   done
-
-  echo -e "${_FG_BLUE}$i_mdi_information_variant Info (3/3):${_TX_RESET} Linking Files within $_SELECTED_ITEM_TEXT was completed!\n"
 }
 
 #
@@ -194,9 +238,16 @@ main() {
 
   prepareDirectories
 
+  echo -e "${_FG_BLUE}$i_mdi_information_variant Info (1/3):${_TX_RESET} Directory prepartions complete!\n"
+
   linkConfigDirectory
 
+  echo -e "${_FG_BLUE}$i_mdi_information_variant Info (2/3):${_TX_RESET} Linkings .config Directory was completed!\n"
+
   linkConfigFiles
+
+  echo -e "${_FG_BLUE}$i_mdi_information_variant Info (3/3):${_TX_RESET} Linking Files within $_SELECTED_ITEM_TEXT was completed!\n"
+  echo -e "${_FG_GREEN}$i_mdi_check Success :${_TX_RESET} setup was completed!\n"
 }
 
 #
@@ -212,14 +263,14 @@ printHelp() {
 # handle script options.
 #
 while getopts ":heq:" opt; do
-	case ${opt} in
-		e  ) main ;;
+  case ${opt} in
+    e  ) main ;;
     q  ) _QUICK_SELECT=true; main "$OPTARG" ;;
-		h  ) printHelp exit 1;;
-		\? ) echo -e "${_FG_YELLOW}Unknown Option: ${_TX_RESET} -$OPTARG" >&2; exit 1;;
-		:  ) echo -e "${_FG_YELLOW}Missing option argument for ${_TX_RESET} -$OPTARG" >&2; exit 1;;
-		*  ) echo -e "${_FG_RED}Unimplemented Option: ${_TX_RESET} -$OPTARG" >&2; exit 1;;
-	esac
+    h  ) printHelp exit 1;;
+    \? ) echo -e "${_FG_YELLOW}Unknown Option: ${_TX_RESET} -$OPTARG" >&2; exit 1;;
+    :  ) echo -e "${_FG_YELLOW}Missing option argument for ${_TX_RESET} -$OPTARG" >&2; exit 1;;
+    *  ) echo -e "${_FG_RED}Unimplemented Option: ${_TX_RESET} -$OPTARG" >&2; exit 1;;
+  esac
 done
 
 # Standard Behaviour when, no option was supplied.
