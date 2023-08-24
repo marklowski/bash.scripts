828e199b6b18eb9da3414c5d17e3140f03a0f53d
Philipp von Marklowski <philipp.vonmarklowski@pr-p.net>
2023-08-25 00:23:16 +0200

    feat(gitNewRepository): add Json Repository Documentation

--- gitNewRepository.sh
+++ gitNewRepository.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
+## Requires: 'JQ' Package to Function correctly
 # Create git repo if not existent
+_CONFIG_SYSTEMS_FILE="$HOME/.config/script-settings/systems.json"
+_CONFIG_OVERVIEW="$HOME/repositories/overview.json"
 
 # Global Variables
 _NEW_REPOSITORY=$1
@@ -17,34 +21,221 @@ _GIT_SUFFIX='.git'
 
 _POST_RECEIVE_HOOK="$HOME/post-receive"
 
-# Split imported String into Folder/Repository
-readarray -d . -t repository_array <<< "$_NEW_REPOSITORY"
-for (( counter=0; counter < ${#repository_array[*]}; counter++))
-do
-    if [ $counter = 0 ]; then
-        _REPOSITORY_PREFIX=$_DIRECTORY_PATH${repository_array[counter]}
+# Title: Utility Functions
+trimWhiteSpaces() {
+    local inputVar="$1"
+    local trimmedVar
+    trimmedVar=$(echo "$inputVar" | tr -d '[:space:]')
+    echo "$trimmedVar"
+}
+
+addGitSuffix() {
+    local inputVar="$1"
+    local varWithSuffix
+    trimmedVar="$inputVar$_GIT_SUFFIX"
+    echo "$trimmedVar"
+}
+
+# Title: Sub Functions
+
+##Split imported String into Folder/Repository
+splitUserInput() {
+    readarray -d . -t repository_array <<< "$_NEW_REPOSITORY"
+    for (( counter=0; counter < ${#repository_array[*]}; counter++))
+    do
+        if [ $counter = 0 ]; then
+            _REPOSITORY_PREFIX=$_DIRECTORY_PATH${repository_array[counter]}
+        else
+            [[ -n $_REPOSITORY_NAME ]] && buffer="$_REPOSITORY_NAME."
+            _REPOSITORY_NAME="$buffer${repository_array[counter]}"
+        fi
+    done
+}
+
+## remove whitespaces and set directory path
+setDirectoryPath() {
+    if [[ "$_REPOSITORY_NAME" == *"$_GIT_SUFFIX"* ]]; then
+			echo -e "${_FG_YELLOW}Invalid Repository Name: ${_TX_RESET} Try Again without '.git'"
+      exit 1
+    fi
+
+    _REPOSITORY_NAME=$(trimWhiteSpaces $_REPOSITORY_NAME)
+
+    repositoryWithSuffix=$(addGitSuffix $_REPOSITORY_NAME)
+    _DIRECTORY_PATH="$_DIRECTORY_PREFIX$_REPOSITORY_PREFIX/$repositoryWithSuffix"
+}
+
+## remove whitespaces and symlink path
+setSymlinkPath() {
+    if [[ "$_NEW_REPOSITORY" == *"$_GIT_SUFFIX"* ]]; then
+			echo -e "${_FG_YELLOW}Invalid Repository Name: ${_TX_RESET} Try Again without '.git'"
+      exit 1
+    fi
+
+    _NEW_REPOSITORY=$(trimWhiteSpaces $_NEW_REPOSITORY)
+
+    repositoryWithSuffix=$(addGitSuffix $_NEW_REPOSITORY)
+    _SYMLINK_PATH="$_SYMLINK_PREFIX$repositoryWithSuffix"
+}
+
+updateJsonRepository() {
+    local group="$1"
+    local repository="$2"
+
+    # Check if required arguments are provided
+    if [[ -z "$group" || -z "$repository" ]]; then
+        echo "Error: Missing arguments."
+        exit 1
+    fi
+
+    # Check if overview.json exists
+    if [[ ! -f "$_CONFIG_OVERVIEW" ]]; then
+        echo "Overview.json not found. Continuing with normal procedure."
+        return 0
+    fi
+
+    # Update properties for specific object if found
+    updated_json=$(jq \
+      --arg group "$group" \
+      --arg repository "$repository" \
+      --arg changeDate "$(date +"%Y-%m-%d")" \
+      --arg changeTime "$(date +"%H:%M:%S")" \
+      '
+        if (.repositories | any(.group == $group and .repository == $repository)) then
+            .repositories |= map(
+                if .group == $group and .repository == $repository then
+                    .changeDate = $changeDate
+                    | .changeTime = $changeTime
+                    | .systems |= map(
+                        . + { "hasChanges": true }
+                    )
+                    else .
+                end
+            )
+            else .
+        end' "$_CONFIG_OVERVIEW")
+
+    if [[ $? != 0 ]]; then
+        echo "Error: Failed to update overview.json."
+        exit 1
+    fi
+
+    if [[ "$updated_json" != "$(cat "$_CONFIG_OVERVIEW")" ]]; then
+        echo "$updated_json" > "$_CONFIG_OVERVIEW"  # Update the overview.json file
+        echo "Updated existing Repository Object."
+        exit 2
     else
-        [[ -n $_REPOSITORY_NAME ]] && buffer="$_REPOSITORY_NAME."
-        _REPOSITORY_NAME="$buffer${repository_array[counter]}"
-    fi 
-done
+        return 0
+    fi
+}
 
-# remove whitespaces and set directory path
-[[ "$_REPOSITORY_NAME" == *"$_GIT_SUFFIX"* ]] || 
-    _REPOSITORY_NAME="$(echo -e "$_REPOSITORY_NAME$_GIT_SUFFIX" | tr -d '[:space:]')" &&
-    _DIRECTORY_PATH="$_DIRECTORY_PREFIX$_REPOSITORY_PREFIX/$_REPOSITORY_NAME"
+## build Json Object for Repository Overview
+buildJsonRepository() {
+    local group="$1"
+    local repository="$2"
+    local systems=$(jq -c '.systems' "$_CONFIG_SYSTEMS_FILE")
 
-# remove whitespaces and symlink path
-[[ "$_NEW_REPOSITORY" == *"$_GIT_SUFFIX"* ]] ||
-    _NEW_REPOSITORY="$(echo -e "$_NEW_REPOSITORY$_GIT_SUFFIX" | tr -d '[:space:]')" &&
-    _SYMLINK_PATH="$_SYMLINK_PREFIX$_NEW_REPOSITORY"
+    # Create the JSON object
+    json_obj=$(jq -n \
+        --arg group "$group" \
+        --arg repository "$repository" \
+        --arg creationDate "$(date +"%Y-%m-%d")" \
+        --arg creationTime "$(date +"%H:%M:%S")" \
+        --arg changeDate "" \
+        --arg changeTime "" \
+        --argjson systems "$systems" \
+        '{
+            "group": $group,
+            "repository": $repository,
+            "creationDate": $creationDate,
+            "creationTime": $creationTime,
+            "changeDate": $changeDate,
+            "changeTime": $changeTime,
+            "systems": $systems
+          }')
 
-# check if Repository exists already
-[[ ! -d "$_DIRECTORY_PATH" ]] && 
-    echo $_DIRECTORY_PATH > $HOME/manageNewProjects.txt && 
-    mkdir -p "$_DIRECTORY_PATH" &&
-    cd $_DIRECTORY_PATH && 
-    git init --bare && 
-    cp $_POST_RECEIVE_HOOK ./hooks &&
-    ln -s $_DIRECTORY_PATH $_SYMLINK_PATH ||
-    echo 'Git Repository already exists'
+    if [[ $? != 0 ]]; then
+        echo "Error: Failed to create JSON object."
+        exit 1
+    fi
+
+    echo "$json_obj"  # Return the JSON object
+}
+
+## add Json Object to Repository Overview
+addJsonRepository() {
+    local json_object="$1"
+
+    # Check if required argument is provided
+    if [[ -z "$json_object" ]]; then
+        echo "Error: Missing JSON object."
+        exit 1
+    fi
+
+    # Update properties for date and time
+    updated_json=$(jq --arg changeDate "$(date +"%Y-%m-%d")" \
+                       --arg changeTime "$(date +"%H:%M:%S")" \
+                       '.changeDate = $changeDate | .changeTime = $changeTime | .repositories += [$json_object]' \
+                       --argjson json_object "$json_object" "$_CONFIG_OVERVIEW")
+
+    if [[ $? != 0 ]]; then
+        echo "Error: Failed to update overview.json."
+        exit 1
+    fi
+
+    echo "$updated_json" > "$_CONFIG_OVERVIEW"  # Update the overview.json file
+}
+
+
+# Title: Execution Functions
+
+## handle Repository preparation
+prepareRepository() {
+    splitUserInput
+    setDirectoryPath
+    setSymlinkPath
+}
+
+## handle JSON Repository creation
+handleJsonRepository() {
+
+    # try to update existing Repository Object
+    updateJsonRepository "$_REPOSITORY_PREFIX" "$_REPOSITORY_NAME"
+
+    # no Repository Object was found, build & create New Repository Object
+    # Call the addJsonRepository function to add the Repository Object to Overview.json
+    jsonRepository=$(buildJsonRepository "$_REPOSITORY_PREFIX" "$_REPOSITORY_NAME")
+    addJsonRepository "$jsonRepository"
+
+    echo "Repository Object added to Overview.json."
+}
+
+## create Repository Directories
+createRepository() {
+    if [[ -d "$_DIRECTORY_PATH" ]]; then
+			echo -e "${_FG_RED}Error: ${_TX_RESET} Git Repository exists already!"
+      exit 1
+    fi
+
+    # create directory with possible parents
+    mkdir -p "$_DIRECTORY_PATH"
+    cd $_DIRECTORY_PATH
+
+    # add git initialization & hooks
+    git init --bare
+    cp $_POST_RECEIVE_HOOK ./hooks
+
+    # add symlink
+    ln -s $_DIRECTORY_PATH $_SYMLINK_PATH
+}
+
+## main Execution
+main() {
+    prepareRepository
+
+    createRepository
+
+    handleJsonRepository
+}
+
+main
